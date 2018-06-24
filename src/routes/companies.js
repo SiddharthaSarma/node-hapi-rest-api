a380aa65b8f2c6b795a12fe570847e8bdf6e8afb
siddharthasarma <sidduinevitable@gmail.com>
2018-06-24 18:17:06 +0530

    Added swagger documentation for the Get companies method

--- src/routes/companies.js
+++ src/routes/companies.js
@@ -1,17 +1,21 @@
 import { getAllCompanies } from '../controller/company';
+
 export const companyRoutes = [
   {
     method: 'GET',
     path: '/companies',
     config: {
+      description: 'Get companies',
+      notes: 'Get the list of companies',
+      tags: ['api'], // ADD THIS TAG
+      async handler(req, res) {
+        return new Promise(async resolve => {
+          return resolve(await getAllCompanies(req, res));
+        });
+      },
       response: {
         emptyStatusCode: 204
       }
-    },
-    async handler(req, res) {
-      return new Promise(async resolve => {
-        return resolve(await getAllCompanies(req, res));
-      });
     }
   }
 ];
--- src/server.js
+++ src/server.js
@@ -1,25 +1,34 @@
 'use strict';
 import Hapi from 'hapi';
+import HapiSwagger from 'hapi-swagger';
+import Inert from 'inert';
+import Vision from 'vision';
 import './database';
 import routes from './routes';
 
-const server = Hapi.server({
+const server = new Hapi.server({
   host: 'localhost',
   port: 8000
 });
 
+const register = server.register([
+  Inert,
+  Vision,
+  {
+    plugin: HapiSwagger
+  }
+]);
 // Add the route
 server.route(routes);
 
 // Start the server
 async function start() {
   try {
+    await register;
     await server.start();
   } catch (err) {
     console.log(err);
-    process.exit(1);
   }
-
   console.log('Server running at:', server.info.uri);
 }
 
